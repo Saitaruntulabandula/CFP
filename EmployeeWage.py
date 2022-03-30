437cf14319662a5e5d7e1dc6deee7e0fcff5efd6
Saitaruntulabandula <saitaruntulabandula@gmail.com>
2022-03-30 07:35:06 +0530

    [Tarun]Add:Part Time Employee Wage

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -3,20 +3,25 @@
 @Date: 2022-03-29 20: 47: 00
 @Last Modified by: Sai Tarun
 @Last Modified time: 2022-03-29 20: 50: 00
-@Title: Check weather the Employee is Present or Absent
+@Title: Check if Employee is Present or Absent
+        Add Part Time Employee and Wage.
 '''
 
 import random
 
-Present=1
+Full_Time_Worker=1
+Part_Time_Worker=2
 Rate_Per_Hour=20
-check_attendance=random.randint(0,1)
+check_attendance=random.randint(0,2)
 
-if check_attendance==1:   
-    print("Employee is Present")
+if check_attendance==Full_Time_Worker:   
+    print("Employee is Full time worker")
     working_hours=8
+elif check_attendance==Part_Time_Worker:
+    print("Employee is Part time worker")
+    working_hours=4
 else:
-    print("Employee is Absent")
     working_hours=0
+    print("Employee is Absent")
 employee_salary=(Rate_Per_Hour*working_hours)
 print(f"Salary:{employee_salary}")
