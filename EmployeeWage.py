3ba804189bba97506d90cd72cbdbdc8c98360c8c
Saitaruntulabandula <saitaruntulabandula@gmail.com>
2022-03-30 15:03:38 +0530

    [Tarun]Add:Max Hours Max Days

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -3,8 +3,7 @@
 @Date: 2022-03-29 20: 47: 00
 @Last Modified by: Sai Tarun
 @Last Modified time: 2022-03-29 20: 50: 00
-@Title: Check if Employee is Present or Absent
-        Calculating Wages per Month.
+@Title: Reaching Max Working Days and Max Working Hours 
 '''
 
 import random
@@ -12,6 +11,7 @@ import random
 #Declaring Variables
 Rate_Per_Hour=20
 Max_Working_Days=20
+Max_Working_Hours_In_Month=100
 Total_Emp_Hours=0
 Total_Working_Days=0
 
@@ -19,7 +19,7 @@ def working_hours_fun(check_attendance):
     switcher={0:0,1:8,2:4}
     return switcher[check_attendance]
 
-while(Total_Working_Days<Max_Working_Days):
+while(Total_Working_Days<Max_Working_Days) and (Total_Emp_Hours<Max_Working_Hours_In_Month):
     Total_Working_Days+=1
     check_attendance=random.randint(0,2)
     working_hours=working_hours_fun(check_attendance)
