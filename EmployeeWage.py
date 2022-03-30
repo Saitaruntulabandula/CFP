08c1ebaa50d3649fe819ae0cc4d772f8155e9775
Saitaruntulabandula <saitaruntulabandula@gmail.com>
2022-03-30 13:05:09 +0530

    [Tarun]Add:Monthly Wage Calculation

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -4,22 +4,26 @@
 @Last Modified by: Sai Tarun
 @Last Modified time: 2022-03-29 20: 50: 00
 @Title: Check if Employee is Present or Absent
-        Using Case Statement
+        Calculating Wages per Month.
 '''
 
 import random
+
+#Declaring Variables
+Rate_Per_Hour=20
+Max_Working_Days=20
+Total_Emp_Hours=0
+Total_Working_Days=0
 
 def working_hours_fun(check_attendance):
     switcher={0:0,1:8,2:4}
     return switcher[check_attendance]
 
+while(Total_Working_Days<Max_Working_Days):
+    Total_Working_Days+=1
+    check_attendance=random.randint(0,2)
+    working_hours=working_hours_fun(check_attendance)
+    Total_Emp_Hours=Total_Emp_Hours+working_hours
 
-
-Full_Time_Worker=1
-Part_Time_Worker=2
-Rate_Per_Hour=20
-check_attendance=random.randint(0,2)
-working_hours=working_hours_fun(check_attendance)
-
-employee_salary=(Rate_Per_Hour*working_hours)
-print(f"Salary:{employee_salary}")
+employee_salary=(Rate_Per_Hour*Total_Emp_Hours)
+print(f"Employee's Monthly Wage is:{employee_salary}")
