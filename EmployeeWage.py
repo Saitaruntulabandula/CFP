e336b5ff7ad41dcf9021bf54e9c19e106835ccbb
Saitaruntulabandula <saitaruntulabandula@gmail.com>
2022-03-30 08:46:56 +0530

    [Tarun]Add:Case Statement

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -4,24 +4,22 @@
 @Last Modified by: Sai Tarun
 @Last Modified time: 2022-03-29 20: 50: 00
 @Title: Check if Employee is Present or Absent
-        Add Part Time Employee and Wage.
+        Using Case Statement
 '''
 
 import random
+
+def working_hours_fun(check_attendance):
+    switcher={0:0,1:8,2:4}
+    return switcher[check_attendance]
+
+
 
 Full_Time_Worker=1
 Part_Time_Worker=2
 Rate_Per_Hour=20
 check_attendance=random.randint(0,2)
+working_hours=working_hours_fun(check_attendance)
 
-if check_attendance==Full_Time_Worker:   
-    print("Employee is Full time worker")
-    working_hours=8
-elif check_attendance==Part_Time_Worker:
-    print("Employee is Part time worker")
-    working_hours=4
-else:
-    working_hours=0
-    print("Employee is Absent")
 employee_salary=(Rate_Per_Hour*working_hours)
 print(f"Salary:{employee_salary}")
