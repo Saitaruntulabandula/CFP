25eec6a69371919ecb26a1d4326fbb2446c5d96c
Saitaruntulabandula <saitaruntulabandula@gmail.com>
2022-04-03 08:09:19 +0530

    [Tarun]Add: Monthly Wage

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,10 +1,10 @@
 '''
 @Author: Sai Tarun
-@Date: 2022-03-31 16: 30: 00
+@Date: 2022-03-04 07: 39: 00
 @Last Modified by: Sai Tarun
-@Last Modified time: 2022-03-31 16: 50: 00
+@Last Modified time: 2022-03-04 07: 45: 00
 @Title: Check if Employee is Present or Absent
-        Using Case Statement
+        Employee monthly wage for 20 working days
 '''
 
 import random
@@ -33,20 +33,30 @@ def attendance(check_attendance):
     switcher={0:0,1:1}
     return switcher[check_attendance]
 
-Rate_Per_Hour=20
-check_attendance=random.randint(0,1)
-attendance_status=attendance(check_attendance)
-if attendance_status==0:
-    print("Employee is absent")
-elif attendance_status==1:
-    print("Employee is present")
-    working_hours_count=random.randint(0,1)
-    working_hours=working_hours_fun(working_hours_count)
-    if working_hours==8:
-        print("Employee is present full time")
-        employee_salary=(Rate_Per_Hour*working_hours)
-        print(f"Salary of the full time employee is:{employee_salary}")
-    else:
-        print("Employee is present part time")
-        employee_salary=(Rate_Per_Hour*working_hours)
-        print(f"Salary of the part time employee is:{employee_salary}")
+rate_per_hour=20
+part_time_count=0
+full_time_count=0
+total_monthly_wage=0
+total_working_days=0
+while total_working_days<20:
+    check_attendance=random.randint(0,1)
+    attendance_status=attendance(check_attendance)
+    if attendance_status==0:
+        print("Employee is absent")
+    elif attendance_status==1:
+        working_hours_count=random.randint(0,1)
+        working_hours=working_hours_fun(working_hours_count)
+        if working_hours==8:
+            print("Employee is present full time")
+            full_time_count=full_time_count+1
+            employee_salary=(rate_per_hour*working_hours)
+        else:
+            print("Employee is present part time")
+            part_time_count=part_time_count+1
+            employee_salary=(rate_per_hour*working_hours)
+        total_monthly_wage=total_monthly_wage+employee_salary
+        total_working_days=total_working_days+1
+print("Total working days is: ",total_working_days)
+print("Part time working days: ",part_time_count)
+print("Full time working days: ",full_time_count)
+print("Total monthly wage: ",total_monthly_wage)
--- Welcome Message.py
+++
@@ -1,1 +0,0 @@
-print("Welcome to Employee Wage Computation")
