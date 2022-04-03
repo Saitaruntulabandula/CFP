dca7f1207444958def57524358f86563866b4ab5
Saitaruntulabandula <saitaruntulabandula@gmail.com>
2022-04-03 11:15:54 +0530

    [Tarun]Add : Reaching Max Hours And Days

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,10 +1,10 @@
 '''
 @Author: Sai Tarun
-@Date: 2022-03-04 07: 39: 00
+@Date: 2022-04-03 11: 11: 00
 @Last Modified by: Sai Tarun
-@Last Modified time: 2022-03-04 07: 45: 00
+@Last Modified time: 2022-04-03 11: 20: 00
 @Title: Check if Employee is Present or Absent
-        Employee monthly wage for 20 working days
+        Reaching Maximum Working Hours and Days
 '''
 
 import random
@@ -33,30 +33,28 @@ def attendance(check_attendance):
     switcher={0:0,1:1}
     return switcher[check_attendance]
 
-rate_per_hour=20
-part_time_count=0
-full_time_count=0
-total_monthly_wage=0
-total_working_days=0
-while total_working_days<20:
+#Declaring Variables
+Rate_Per_Hour=20
+Max_Working_Days=20
+Max_Working_Hours_In_Month=100
+Total_Emp_Hours=0
+Total_Working_Days=0
+while(Total_Working_Days<Max_Working_Days) or (Total_Emp_Hours<Max_Working_Hours_In_Month):
     check_attendance=random.randint(0,1)
     attendance_status=attendance(check_attendance)
     if attendance_status==0:
-        print("Employee is absent")
+        Total_Emp_Hours=Total_Emp_Hours+0
     elif attendance_status==1:
         working_hours_count=random.randint(0,1)
         working_hours=working_hours_fun(working_hours_count)
         if working_hours==8:
-            print("Employee is present full time")
-            full_time_count=full_time_count+1
-            employee_salary=(rate_per_hour*working_hours)
+            Total_Emp_Hours=Total_Emp_Hours+8
         else:
-            print("Employee is present part time")
-            part_time_count=part_time_count+1
-            employee_salary=(rate_per_hour*working_hours)
-        total_monthly_wage=total_monthly_wage+employee_salary
-        total_working_days=total_working_days+1
-print("Total working days is: ",total_working_days)
-print("Part time working days: ",part_time_count)
-print("Full time working days: ",full_time_count)
-print("Total monthly wage: ",total_monthly_wage)
+            Total_Emp_Hours=Total_Emp_Hours+4
+        Total_Working_Days=Total_Working_Days+1
+
+
+employee_salary=(Rate_Per_Hour*Total_Emp_Hours)
+print("Total working days: ",Total_Working_Days)
+print("Total emp working hours: ",Total_Emp_Hours)
+print(f"Employee's Monthly Wage is :{employee_salary}")
