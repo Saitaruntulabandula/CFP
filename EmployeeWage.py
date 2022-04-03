da3e5bcafc45cfb1031047f8f257223a08c58b70
Saitaruntulabandula <saitaruntulabandula@gmail.com>
2022-04-03 12:49:27 +0530

    [Tarun]Add: Day Daily Wage Total Wage

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,14 +1,16 @@
 '''
 @Author: Sai Tarun
-@Date: 2022-03-30 20: 47: 00
+@Date: 2022-04-03 11: 11: 00
 @Last Modified by: Sai Tarun
-@Last Modified time: 2022-03-30 20: 50: 00
-@Title: Daily Wage and Total Wage.
+@Last Modified time: 2022-04-03 11: 20: 00
+@Title: Check if Employee is Present or Absent
+        Using Function to Daily Wage and Total Wage
+        Reaching Maximum Working Hours and Days
 '''
 
 import random
 
-def working_hours_fun(check_attendance):
+def working_hours_fun(working_hours_count):
     """
         Description:
             Function is used to check Working Hours.
@@ -17,29 +19,56 @@ def working_hours_fun(check_attendance):
         Return:
             Returns the value of working hours
     """
-    switcher={0:0,1:8,2:4}
+    switcher={0:8,1:4}
+    return switcher[working_hours_count]
+
+def attendance(check_attendance):
+    """
+        Description:
+            Function is used to check attendance.
+        Parameter:
+            Random integer is used as a parameter. 
+        Return:
+            Returns the value of the random integer key.
+    """
+    switcher={0:0,1:1}
     return switcher[check_attendance]
 def cal_hours():
     """
         Description:
-            Function is used to calculate daily wage and total wage
+            Function is used to Calculate Working Hours.
         Parameter:
-            No parameter is required 
+            Not passed any parameter 
         Return:
-            Returns nothing,but prints daily wage and total wage
+            Returns nothing but prints daily wage and total wage.
     """
-    Rate_Per_Hour=20
-    Max_Working_Days=20
-    Max_Working_Hours_In_Month=100
-    Total_Emp_Hours=0
-    Total_Working_Days=0
-    while(Total_Working_Days<Max_Working_Days) and (Total_Emp_Hours<Max_Working_Hours_In_Month):
-        Total_Working_Days+=1
-        check_attendance=random.randint(0,2)
-        working_hours=working_hours_fun(check_attendance)
-        Daily_Wage=working_hours*Rate_Per_Hour
-        print("For day",Total_Working_Days,"Daily Wage of Employee is ",Daily_Wage)
-        Total_Emp_Hours=Total_Emp_Hours+working_hours
-    employee_salary=(Rate_Per_Hour*Total_Emp_Hours)
-    print("Employee's Total Wage is:",employee_salary)
+    #Declaring Variables
+    rate_per_hour=20
+    max_working_days=20
+    max_working_hours_in_month=100
+    total_emp_hours=0
+    total_working_days=0
+    total_wage=0
+    while(total_working_days<max_working_days) or (total_emp_hours<max_working_hours_in_month):
+        check_attendance=random.randint(0,1)
+        attendance_status=attendance(check_attendance)
+        if attendance_status==0:
+            pass
+        elif attendance_status==1:
+            working_hours_count=random.randint(0,1)
+            working_hours=working_hours_fun(working_hours_count)
+            total_working_days=total_working_days+1
+            print("For Working day",total_working_days)
+            if working_hours==8:
+                daily_wage=working_hours*rate_per_hour
+                print("Daily Wage of Employee is ",daily_wage)
+                total_emp_hours=total_emp_hours+8
+            else:
+                daily_wage=working_hours*rate_per_hour
+                print("Daily Wage of Employee is ",daily_wage)
+                total_emp_hours=total_emp_hours+4
+            total_wage=total_wage+daily_wage
+    print("Total emp wage:  ",total_wage)
+    print("Total working days:  ",total_working_days)
+    print("Total emp working hours: ",total_emp_hours)
 cal_hours()
